6d24c676dc78d36546d978236d49dc1822d46c15
Semih Şahinoğlu <semihgtt11@gmail.com>
2025-06-15 13:16:02 +0300

    eklendi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [formData, setFormData] = useState({ ad: "", soyad: "", yas: 0 });
+
+  const handleChange = (e) => {
+    setFormData((data) => ({
+      ...data,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="ad">Ad</label>
+          <input
+            name="ad"
+            id="ad"
+            type="text"
+            value={formData.ad}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="soyad">Soyad</label>
+          <input
+            name="soyad"
+            id="soyad"
+            type="text"
+            value={formData.soyad}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="yas">Yas</label>
+          <input
+            name="yas"
+            id="yas"
+            type="number"
+            value={formData.yas}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
